f0c98ed77ba887bd8eedde54a4b5674c6a4c4128
arjunmudaliyar747 <arjunmudaliyar99@gmail.com>
2026-02-24 17:11:14 +0530

    nnn

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://192.168.0.250:5000/api';
 
 const tabs = document.querySelectorAll('.tab');
 const loginForm = document.getElementById('loginForm');
